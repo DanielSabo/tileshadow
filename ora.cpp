d7ee6694cdf78af4b69ab3e71268d261ae750170
Daniel Sabo <DanielSabo@gmail.com>
2014-05-24 23:09:12 -0700

    saving: Clean up ORA code and deal with empty layers

--- ora.cpp
+++ ora.cpp
@@ -57,79 +57,71 @@ void saveStackAs(CanvasStack *stack, QString path)
 
     for (layersIter = stack->layers.begin(); layersIter != stack->layers.end(); layersIter++)
     {
-        //FIXME: Still need to write somethign to the ORA even if the layer is empty
+        QRect bounds;
+        uint16_t *layerData = NULL;
+
         if ((*layersIter)->tiles.empty())
-            continue;
-
-        TileMap::iterator tilesIter;
-
-        tilesIter = (*layersIter)->tiles.begin();
-        int x = tilesIter->first.x();
-        int y = tilesIter->first.y();
-
-        QRect tileBounds = QRect(x, y, 1, 1);
-
-        x *= TILE_PIXEL_WIDTH;
-        y *= TILE_PIXEL_HEIGHT;
-
-        QRect bounds = QRect(x, y, TILE_PIXEL_WIDTH, TILE_PIXEL_HEIGHT);
-        tilesIter++;
-
-        for (; tilesIter != (*layersIter)->tiles.end(); tilesIter++)
         {
-            x = tilesIter->first.x();
-            y = tilesIter->first.y();
-
-            tileBounds = tileBounds.united(QRect(x, y, 1, 1));
-
-            x *= TILE_PIXEL_WIDTH;
-            y *= TILE_PIXEL_HEIGHT;
-
-            bounds = bounds.united(QRect(x, y, TILE_PIXEL_WIDTH, TILE_PIXEL_HEIGHT));
+            bounds = QRect(0, 0, 1, 1);
+            layerData = new uint16_t[bounds.width() * bounds.height() * 4];
+            memset(layerData, 0, bounds.width() * bounds.height() * 4 * sizeof(uint16_t));
         }
+        else
+        {
+            TileMap::iterator tilesIter;
 
-        qDebug() << "Layer bounds" << bounds;
-        qDebug() << "Tile bounds" << tileBounds;
+            tilesIter = (*layersIter)->tiles.begin();
 
-        // Linearize image
-        uint16_t *layerData = new uint16_t[bounds.width() * bounds.height() * 4];
-        size_t rowComps = bounds.width() * 4;
+            QRect tileBounds = QRect(tilesIter->first.x(), tilesIter->first.y(), 1, 1);
 
-        for (int iy = 0; iy < tileBounds.height(); ++iy)
-            for (int ix = 0; ix < tileBounds.width(); ++ix)
+            tilesIter++;
+
+            for (; tilesIter != (*layersIter)->tiles.end(); tilesIter++)
             {
-                CanvasTile *tile = (*layersIter)->getTileMaybe(ix + tileBounds.x(), iy + tileBounds.y());
-                uint16_t *rowPtr = layerData + (rowComps * iy * TILE_PIXEL_HEIGHT)
-                                             + (4 * ix * TILE_PIXEL_WIDTH);
-
-                if (tile)
-                {
-                    tile->mapHost();
-                    float *tileData = tile->tileData;
-
-                    for (int row = 0; row < TILE_PIXEL_HEIGHT; row++)
-                    {
-                        for (int col = 0; col < TILE_PIXEL_WIDTH; col++)
-                        {
-                            rowPtr[col * 4 + 0] = qToBigEndian((uint16_t)(tileData[col * 4 + 0] * 0xFFFF));
-                            rowPtr[col * 4 + 1] = qToBigEndian((uint16_t)(tileData[col * 4 + 1] * 0xFFFF));
-                            rowPtr[col * 4 + 2] = qToBigEndian((uint16_t)(tileData[col * 4 + 2] * 0xFFFF));
-                            rowPtr[col * 4 + 3] = qToBigEndian((uint16_t)(tileData[col * 4 + 3] * 0xFFFF));
-                        }
-
-                        rowPtr += rowComps;
-                        tileData += TILE_PIXEL_WIDTH * 4;
-                    }
-                }
-                else
-                {
-                    for (int row = 0; row < TILE_PIXEL_HEIGHT; row++)
-                    {
-                        memset(rowPtr, 0, TILE_PIXEL_WIDTH * sizeof(uint16_t) * 4);
-                        rowPtr += rowComps;
-                    }
-                }
+                tileBounds = tileBounds.united(QRect(tilesIter->first.x(), tilesIter->first.y(), 1, 1));
             }
+
+            bounds = QRect(tileBounds.x() * TILE_PIXEL_WIDTH,
+                           tileBounds.y() * TILE_PIXEL_HEIGHT,
+                           tileBounds.width() * TILE_PIXEL_WIDTH,
+                           tileBounds.height() * TILE_PIXEL_HEIGHT);
+
+            // Linearize image
+            layerData = new uint16_t[bounds.width() * bounds.height() * 4];
+            size_t rowComps = bounds.width() * 4;
+
+            for (int iy = 0; iy < tileBounds.height(); ++iy)
+                for (int ix = 0; ix < tileBounds.width(); ++ix)
+                {
+                    CanvasTile *tile = (*layersIter)->getTileMaybe(ix + tileBounds.x(), iy + tileBounds.y());
+                    uint16_t *rowPtr = layerData + (rowComps * iy * TILE_PIXEL_HEIGHT)
+                                                 + (4 * ix * TILE_PIXEL_WIDTH);
+                    if (tile)
+                    {
+                        tile->mapHost();
+                        float *tileData = tile->tileData;
+
+                        for (int row = 0; row < TILE_PIXEL_HEIGHT; row++)
+                        {
+                            for (int col = 0; col < TILE_PIXEL_WIDTH * 4; col++)
+                            {
+                                rowPtr[col] = qToBigEndian((uint16_t)(tileData[col] * 0xFFFF));
+                            }
+
+                            rowPtr += rowComps;
+                            tileData += TILE_PIXEL_WIDTH * 4;
+                        }
+                    }
+                    else
+                    {
+                        for (int row = 0; row < TILE_PIXEL_HEIGHT; row++)
+                        {
+                            memset(rowPtr, 0, TILE_PIXEL_WIDTH * sizeof(uint16_t) * 4);
+                            rowPtr += rowComps;
+                        }
+                    }
+                }
+        }
 
         unsigned char *pngData;
         size_t pngDataSize;
@@ -142,8 +134,6 @@ void saveStackAs(CanvasStack *stack, QString path)
         QString layerFileName = QString().sprintf("layer%03d.png", layerNum++);
 
         oraZipWriter.addFile(layerFileName, QByteArray::fromRawData((const char *)pngData, pngDataSize));
-
-        qDebug() << layerFileName;
 
         free(pngData);
         delete[] layerData;
@@ -167,8 +157,5 @@ void saveStackAs(CanvasStack *stack, QString path)
 
     oraZipWriter.addFile("stack.xml", stackBuffer.buffer());
     QByteArray mimetypeData("image/openraster");
-    mimetypeData.chop(1); // Remove nul terminator
     oraZipWriter.addFile("mimetype", mimetypeData);
-
-    qDebug() << stackBuffer.buffer();
 }
