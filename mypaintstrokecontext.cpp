DanielSabo/tileshadow

commit 5ca17a102eb905602e2f4d1898d83361027e5b16

Pass timestamp info to strokeTo()
Which then replaces the timer in MyPaintStrokeContext.

mypaintstrokecontext.cpp
@@ -1,6 +1,5 @@
 #include "mypaintstrokecontext.h"
 #include "canvastile.h"
-#include <QElapsedTimer>
 #include <cstring>
 #include <iostream>
 #include <cmath>
@@ -40,7 +39,6 @@ class MyPaintStrokeContextPrivate
 public:
     MyPaintBrush         *brush;
     CanvasMyPaintSurface *surface;
-    QElapsedTimer         timer;
     TileSet               modTiles;
 };
 
@@ -152,33 +150,29 @@ MyPaintStrokeContext::~MyPaintStrokeContext()
 
 TileSet MyPaintStrokeContext::startStroke(QPointF point, float pressure)
 {
-    (void)point; (void)pressure;
-
     mypaint_brush_reset (priv->brush);
     mypaint_brush_new_stroke(priv->brush);
 
     mypaint_brush_stroke_to(priv->brush, (MyPaintSurface *)priv->surface,
                             point.x(), point.y(),
                             0.0f /* pressure */, 0.0f /* xtilt */, 0.0f /* ytilt */,
-                            1000.0f / 60.0f /* deltaTime in ms*/);
+                            1.0f / 60.0f /* deltaTime in seconds */);
 
     mypaint_brush_stroke_to(priv->brush, (MyPaintSurface *)priv->surface,
                             point.x(), point.y(),
                             pressure /* pressure */, 0.0f /* xtilt */, 0.0f /* ytilt */,
-                            1000.0f / 60.0f /* deltaTime in ms*/);
-    priv->timer.start();
+                            1.0f / 60.0f /* deltaTime in seconds */);
     priv->modTiles.clear();
     return TileSet();
 }
 
 
-TileSet MyPaintStrokeContext::strokeTo(QPointF point, float pressure)
+TileSet MyPaintStrokeContext::strokeTo(QPointF point, float pressure, float dt)
 {
-    double dt = priv->timer.restart() / 1000.0;
     mypaint_brush_stroke_to(priv->brush, (MyPaintSurface *)priv->surface,
                             point.x(), point.y(),
                             pressure /* pressure */, 0.0f /* xtilt */, 0.0f /* ytilt */,
-                            dt /* deltaTime in ms*/);
+                            dt / 1000.0f /* deltaTime in seconds */);
 
     TileSet result = priv->modTiles;
     priv->modTiles.clear();

basicstrokecontext.cpp
@@ -69,7 +69,7 @@ TileSet BasicStrokeContext::startStroke(QPointF point, float pressure)
     return modTiles;
 }
 
-TileSet BasicStrokeContext::strokeTo(QPointF point, float pressure)
+TileSet BasicStrokeContext::strokeTo(QPointF point, float pressure, float dt)
 {
     TileSet modTiles;
     (void)pressure;

basicstrokecontext.h
@@ -9,7 +9,7 @@ public:
     BasicStrokeContext(CanvasContext *ctx, CanvasLayer *layer, float radius);
 
     TileSet startStroke(QPointF point, float pressure);
-    TileSet strokeTo(QPointF point, float pressure);
+    TileSet strokeTo(QPointF point, float pressure, float dt);
     void drawDab(QPointF point, TileSet &modTiles);
 
     void multiplySize(float mult);

canvascontext.h
@@ -20,7 +20,7 @@ public:
     virtual ~StrokeContext() {}
 
     virtual TileSet startStroke(QPointF point, float pressure) = 0;
-    virtual TileSet strokeTo(QPointF point, float pressure) = 0;
+    virtual TileSet strokeTo(QPointF point, float pressure, float dt) = 0;
 
     CanvasContext *ctx;
     CanvasLayer   *layer;
@@ -65,6 +65,7 @@ public:
     int tileHeight;
 
     QScopedPointer<StrokeContext> stroke;
+    ulong strokeEventTimestamp; // last stroke event time, in milliseconds
 
     int currentLayer;
     QScopedPointer<CanvasLayer> currentLayerCopy;

canvaswidget.cpp
@@ -364,13 +364,13 @@ void CanvasWidget::startStroke(QPointF pos, float pressure)
     emit canvasModified();
 }
 
-void CanvasWidget::strokeTo(QPointF pos, float pressure)
+void CanvasWidget::strokeTo(QPointF pos, float pressure, float dt)
 {
     mouseEventRate.addEvents(1);
     TileSet changedTiles;
     if (!ctx->stroke.isNull())
     {
-        changedTiles = ctx->stroke->strokeTo(pos, pressure);
+        changedTiles = ctx->stroke->strokeTo(pos, pressure, dt);
     }
 
     if(!changedTiles.empty())
@@ -805,6 +805,8 @@ void CanvasWidget::mousePressEvent(QMouseEvent *event)
         }
         else if (action == CanvasAction::None)
         {
+            ctx->strokeEventTimestamp = event->timestamp();
+
             startStroke(pos, 1.0f);
             action = CanvasAction::MouseStroke;
         }
@@ -847,8 +849,11 @@ void CanvasWidget::mouseMoveEvent(QMouseEvent *event)
     if (action == CanvasAction::MouseStroke)
     {
         QPointF pos = (event->localPos() + canvasOrigin) / viewScale;
+        ulong newTimestamp = event->timestamp();
 
-        strokeTo(pos, 1.0f);
+        strokeTo(pos, 1.0f, float(newTimestamp) - ctx->strokeEventTimestamp);
+
+        ctx->strokeEventTimestamp = newTimestamp;
     }
     else if (action == CanvasAction::MoveView)
     {
@@ -878,6 +883,7 @@ void CanvasWidget::tabletEvent(QTabletEvent *event)
         }
         else if (action == CanvasAction::None)
         {
+            ctx->strokeEventTimestamp = event->timestamp();
             startStroke(point, event->pressure());
             action = CanvasAction::TabletStroke;
         }
@@ -895,7 +901,11 @@ void CanvasWidget::tabletEvent(QTabletEvent *event)
         updateModifiers(event);
 
         if (action == CanvasAction::TabletStroke)
-            strokeTo(point, event->pressure());
+        {
+            ulong newTimestamp = event->timestamp();
+            strokeTo(point, event->pressure(), float(newTimestamp) - ctx->strokeEventTimestamp);
+            ctx->strokeEventTimestamp = newTimestamp;
+        }
     }
     else
         return;

canvaswidget.h
@@ -43,7 +43,7 @@ public:
     void setActiveTool(const QString &toolName);
     QString getActiveTool();
     void startStroke(QPointF pos, float pressure);
-    void strokeTo(QPointF pos, float pressure);
+    void strokeTo(QPointF pos, float pressure, float dt);
     void endStroke();
 
     float getScale();

mainwindow.cpp
@@ -329,7 +329,7 @@ double drawBenchmarkCircle(CanvasWidget *canvas, float radius, float centerX, fl
     for (int i = 0; i < numPoints; ++i)
     {
         currentAngle += M_PI * 2.0f / 100.0f;
-        canvas->strokeTo(QPointF(cosf(currentAngle) * radius + centerX, sinf(currentAngle) * radius + centerY), 1.0f);
+        canvas->strokeTo(QPointF(cosf(currentAngle) * radius + centerX, sinf(currentAngle) * radius + centerY), 1.0f, 1000.0f / 60.0f);
     }
     canvas->endStroke();
     clFinish(SharedOpenCL::getSharedOpenCL()->cmdQueue);

mypaintstrokecontext.h
@@ -13,7 +13,7 @@ public:
     ~MyPaintStrokeContext();
 
     TileSet startStroke(QPointF point, float pressure);
-    TileSet strokeTo(QPointF point, float pressure);
+    TileSet strokeTo(QPointF point, float pressure, float dt);
 
     bool fromSettings(MyPaintToolSettings const &settings);
     void fromDefaults();

commit 90d0e833343af4dccf35bfbdf166f83b32c15f23

Fix timestamp issue with mypaint stroke
introduced in 5ca17a102eb905602e2f4d1898d83361027e5b16, the time for the
zero pressure event needs to be large to make the pencil brush work
correctly.

mypaintstrokecontext.cpp
@@ -156,7 +156,7 @@ TileSet MyPaintStrokeContext::startStroke(QPointF point, float pressure)
     mypaint_brush_stroke_to(priv->brush, (MyPaintSurface *)priv->surface,
                             point.x(), point.y(),
                             0.0f /* pressure */, 0.0f /* xtilt */, 0.0f /* ytilt */,
-                            1.0f / 60.0f /* deltaTime in seconds */);
+                            1.0f /* deltaTime in seconds */);
 
     mypaint_brush_stroke_to(priv->brush, (MyPaintSurface *)priv->surface,
                             point.x(), point.y(),