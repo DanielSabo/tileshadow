DanielSabo/tileshadow

commit 2143672a9e1f579839d2a0d1df3a8bdf830e0bc4

Use unique_ptr for TileMap

canvasundoevent.cpp
@@ -18,34 +18,26 @@ CanvasUndoTiles::CanvasUndoTiles()
 
 CanvasUndoTiles::~CanvasUndoTiles()
 {
-    for (TileMap::iterator iter = tiles.begin(); iter != tiles.end(); ++iter)
-        delete iter->second;
     tiles.clear();
 }
 
 TileSet CanvasUndoTiles::apply(CanvasStack *stack, int *activeLayer)
 {
     TileSet modifiedTiles;
-    TileMap redoTiles;
 
     for (TileMap::iterator iter = tiles.begin(); iter != tiles.end(); ++iter)
     {
-        CanvasTile *redoTile = (*targetTileMap)[iter->first];
+        std::unique_ptr<CanvasTile> &target = (*targetTileMap)[iter->first];
 
-        if (redoTile)
-            redoTile->swapHost();
+        target->swapHost();
 
-        redoTiles[iter->first] = redoTile;
+        std::swap(target, iter->second);
 
-        if (iter->second)
-            (*targetTileMap)[iter->first] = iter->second;
-        else
+        if (!target)
             targetTileMap->erase(iter->first);
 
         modifiedTiles.insert(iter->first);
     }
-
-    tiles = redoTiles;
 
     std::swap(*activeLayer, currentLayer);
     return modifiedTiles;

canvaseventthread.cpp
@@ -112,17 +112,14 @@ void CanvasEventThread::run()
                     int x = iter.x();
                     int y = iter.y();
 
-                    newTiles[QPoint(x, y)] = ctx->layers.getTileMaybe(x, y).release();
+                    newTiles[QPoint(x, y)] = ctx->layers.getTileMaybe(x, y);
                 }
                 ctx->dirtyTiles.clear();
 
                 resultTilesMutex.lock();
                 for (auto &iter: newTiles)
                 {
-                    CanvasTile *& tile = resultTiles[iter.first];
-                    if (tile)
-                        delete tile;
-                    tile = iter.second;
+                    resultTiles[iter.first] = std::move(iter.second);
                 }
                 newTiles.clear();
                 needResultTiles = false;

canvaslayer.cpp
@@ -2,12 +2,13 @@
 #include "canvastile.h"
 #include "canvascontext.h"
 #include <QDebug>
+#include <utility>
 
 CanvasLayer::CanvasLayer(QString name)
     : name(name),
       visible(true),
       mode(BlendMode::Over),
-      tiles(new TileMap, _deleteTileMap)
+      tiles(new TileMap)
 {
 }
 
@@ -44,10 +45,7 @@ void CanvasLayer::prune()
                 empty = false;
 
         if (empty)
-        {
-            delete iter->second;
             tiles->erase(iter++);
-        }
         else
             ++iter;
     }
@@ -55,7 +53,7 @@ void CanvasLayer::prune()
 
 void CanvasLayer::swapOut()
 {
-    for (auto iter: *tiles)
+    for (auto &iter: *tiles)
         iter.second->swapHost();
 }
 
@@ -132,22 +130,13 @@ CanvasLayer *CanvasLayer::translated(int x, int y) const
 
 TileSet CanvasLayer::takeTiles(CanvasLayer *source)
 {
-    TileMap::iterator iter;
     TileSet result;
 
-    for (TileMap::iterator iter = tiles->begin(); iter != tiles->end(); ++iter)
-    {
-        result.insert(iter->first);
-        delete iter->second;
-    }
     tiles->clear();
+    std::swap(*tiles, *(source->tiles));
 
-    for (iter = source->tiles->begin(); iter != source->tiles->end(); ++iter)
-    {
-        result.insert(iter->first);
-        (*tiles)[iter->first] = iter->second;
-    }
-    source->tiles->clear();
+    for (auto const &iter: *tiles)
+        result.insert(iter.first);
 
     return result;
 }
@@ -165,13 +154,13 @@ TileSet CanvasLayer::getTileSet() const
     return result;
 }
 
-CanvasTile *CanvasLayer::takeTileMaybe(int x, int y)
+std::unique_ptr<CanvasTile> CanvasLayer::takeTileMaybe(int x, int y)
 {
     TileMap::iterator found = tiles->find(QPoint(x, y));
 
     if (found != tiles->end())
     {
-        CanvasTile *result = found->second;
+        std::unique_ptr<CanvasTile> result = std::move(found->second);
         tiles->erase(found);
         return result;
     }
@@ -184,7 +173,7 @@ CanvasTile *CanvasLayer::getTileMaybe(int x, int y) const
     TileMap::iterator found = tiles->find(QPoint(x, y));
 
     if (found != tiles->end())
-        return found->second;
+        return found->second.get();
     else
         return nullptr;
 }
@@ -194,13 +183,13 @@ CanvasTile *CanvasLayer::getTile(int x, int y)
     TileMap::iterator found = tiles->find(QPoint(x, y));
 
     if (found != tiles->end())
-        return found->second;
+        return found->second.get();
 
     CanvasTile *tile = new CanvasTile();
 
     tile->fill(0.0f, 0.0f, 0.0f, 0.0f);
 
-    (*tiles)[QPoint(x, y)] = tile;
+    (*tiles)[QPoint(x, y)] = std::unique_ptr<CanvasTile>(tile);
 
     return tile;
 }

canvaslayer.h
@@ -27,7 +27,7 @@ public:
 
     CanvasTile *getTile(int x, int y);
     CanvasTile *getTileMaybe(int x, int y) const;
-    CanvasTile *takeTileMaybe(int x, int y);
+    std::unique_ptr<CanvasTile> takeTileMaybe(int x, int y);
 
     CanvasLayer *deepCopy() const;
     CanvasLayer *translated(int x, int y) const;

canvasrender.cpp
@@ -278,7 +278,7 @@ void CanvasRender::ensureTiles(TileMap const &tiles)
     if (tiles.empty())
         return;
 
-    for (auto iter: tiles)
+    for (auto &iter: tiles)
     {
         GLuint &ref = glTiles[iter.first];
 
@@ -313,12 +313,9 @@ void CanvasRender::renderTileMap(TileMap &tiles)
 
     ensureTiles(tiles);
 
-    for (auto iter: tiles)
-    {
-        renderTile(iter.first.x(), iter.first.y(), iter.second);
-        if (iter.second)
-            delete iter.second;
-    }
+    for (auto &iter: tiles)
+        renderTile(iter.first.x(), iter.first.y(), iter.second.get());
+
     tiles.clear();
 
     if (SharedOpenCL::getSharedOpenCL()->gl_sharing)

canvasstack.cpp
@@ -6,7 +6,7 @@ CanvasStack::CanvasStack()
 {
     backgroundTile.reset(new CanvasTile());
     backgroundTile->fill(1.0f, 1.0f, 1.0f, 1.0f);
-    backgroundTileCL.reset(backgroundTile->copy());
+    backgroundTileCL = backgroundTile->copy();
     backgroundTileCL->unmapHost();
 }
 
@@ -105,7 +105,7 @@ std::unique_ptr<CanvasTile> CanvasStack::getTileMaybe(int x, int y) const
 {
     int layerCount = layers.size();
 
-    CanvasTile *result = nullptr;
+    std::unique_ptr<CanvasTile> result(nullptr);
 
     if (layerCount == 0)
         result = nullptr;
@@ -120,15 +120,13 @@ std::unique_ptr<CanvasTile> CanvasStack::getTileMaybe(int x, int y) const
         if (auxTile)
         {
             result = backgroundTileCL->copy();
-            clBlendInPlace(result, auxTile, layer->mode);
+            clBlendInPlace(result.get(), auxTile, layer->mode);
         }
         else
             result = nullptr;
     }
     else
     {
-        CanvasTile *inTile = nullptr;
-
         for (CanvasLayer *layer: layers)
         {
             CanvasTile *auxTile = nullptr;
@@ -138,16 +136,14 @@ std::unique_ptr<CanvasTile> CanvasStack::getTileMaybe(int x, int y) const
 
             if (auxTile)
             {
-                if (!inTile)
-                    inTile = backgroundTileCL->copy();
-                clBlendInPlace(inTile, auxTile, layer->mode);
+                if (!result)
+                    result = backgroundTileCL->copy();
+                clBlendInPlace(result.get(), auxTile, layer->mode);
             }
         }
-
-        result = inTile;
     }
 
-    return std::unique_ptr<CanvasTile>(result);
+    return result;
 }
 
 TileSet CanvasStack::getTileSet() const

canvastile.cpp
@@ -115,7 +115,7 @@ void CanvasTile::fill(float r, float g, float b, float a)
                            0, nullptr, nullptr);
 }
 
-CanvasTile *CanvasTile::copy()
+std::unique_ptr<CanvasTile> CanvasTile::copy()
 {
     CanvasTile *result = new CanvasTile();
 
@@ -127,5 +127,5 @@ CanvasTile *CanvasTile::copy()
                         TILE_COMP_TOTAL * sizeof(float),
                         0, nullptr, nullptr);
 
-    return result;
+    return std::unique_ptr<CanvasTile>(result);
 }

canvastile.h
@@ -1,6 +1,8 @@
 #ifndef CANVASTILE_H
 #define CANVASTILE_H
 
+#include <memory>
+
 #ifdef __APPLE__
 #include <OpenCL/cl.h>
 #else
@@ -31,7 +33,7 @@ public:
     void swapHost();
 
     void fill(float r, float g, float b, float a);
-    CanvasTile *copy();
+    std::unique_ptr<CanvasTile> copy();
 
     static int allocatedTileCount();
     static int deviceTileCount();

commit 05bb6df716d688387273167a78d2e8c6ae22f055

Don't try to swap out null tiles in redo
Bug introduced in 2143672a9e1f579839d2a0d1df3a8bdf830e0bc4.

canvasundoevent.cpp
@@ -29,7 +29,8 @@ TileSet CanvasUndoTiles::apply(CanvasStack *stack, int *activeLayer)
     {
         std::unique_ptr<CanvasTile> &target = (*targetTileMap)[iter->first];
 
-        target->swapHost();
+        if (target)
+            target->swapHost();
 
         std::swap(target, iter->second);
 