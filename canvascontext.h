8aa6d566379f68c3c5758d799cd2e97c7f9ea28d
Daniel Sabo <DanielSabo@gmail.com>
2014-11-23 16:26:44 -0800

    Clean up canvascontext.h includes

--- canvascontext.h
+++ canvascontext.h
@@ -1,11 +1,8 @@
 #ifndef CANVASCONTEXT_H
 #define CANVASCONTEXT_H
 
-#include "canvaswidget-opencl.h"
-#include <QPointF>
-#include <QSet>
-#include <QOpenGLFunctions_3_2_Core>
-#include <map>
+#include <QList>
+#include <QScopedPointer>
 #include "tileset.h"
 #include "canvaslayer.h"
 #include "canvasstack.h"
