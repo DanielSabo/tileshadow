13ab060f0758f9781abe0aced6725a13c7b1b993
Daniel Sabo <DanielSabo@gmail.com>
2014-12-11 07:02:12 -0800

    Reformat drawDabFunction()

--- mypaintstrokecontext.cpp
+++ mypaintstrokecontext.cpp
@@ -332,7 +332,8 @@ static int drawDabFunction (MyPaintSurface *base_surface,
                             float lock_alpha,
                             float colorize)
 {
-    CanvasMyPaintSurface *surface = (CanvasMyPaintSurface *)base_surface;
+    if (radius < 1.0f)
+        return 0;
 
     if (hardness < 0.0f)
         hardness = 0.0f;
@@ -348,105 +349,91 @@ static int drawDabFunction (MyPaintSurface *base_surface,
     if (colorize != 0.0f)
         qWarning() << "drawDab called with unsupported values colorize = " << colorize << endl;
 
-    // cout << "Invoked drawDabFunction at " << x << ", " << y << endl;
+    CanvasMyPaintSurface *surface = (CanvasMyPaintSurface *)base_surface;
+    CanvasLayer *layer = surface->strokeContext->layer;
+    QRectF boundRect;
+    cl_kernel kernel;
+    cl_int err = CL_SUCCESS;
+    (void)err; /* Ignore the fact that err is unused, it's helpful for debugging */
 
-    if (radius >= 1.0f)
+    QMatrix transform;
+    transform.scale(1.0f, aspect_ratio);
+    transform.scale(1.0f / radius, 1.0f / radius);
+    transform.rotate(-angle);
+
+    boundRect.setRect(-1.0f, -1.0f, 2.0f, 2.0f);
+    boundRect = transform.inverted().mapRect(boundRect).adjusted(-2.0, -2.0, 4.0, 4.0);
+
+    if (lock_alpha > 0.0f)
+        kernel = SharedOpenCL::getSharedOpenCL()->mypaintDabLockedKernel;
+    else
+        kernel = SharedOpenCL::getSharedOpenCL()->mypaintDabKernel;
+
+    cl_float4 color = {color_r, color_g, color_b, opaque};
+    cl_float4 transformMatrix;
+    transformMatrix.s[0] = transform.m11();
+    transformMatrix.s[1] = transform.m21();
+    transformMatrix.s[2] = transform.m12();
+    transformMatrix.s[3] = transform.m22();
+
+    float slope1 = -(1.0f / hardness - 1.0f);
+    float slope2 = -(hardness / (1.0f - hardness));
+
+    err = clSetKernelArg<cl_float>(kernel, 4, hardness);
+    err = clSetKernelArg<cl_float4>(kernel, 5, transformMatrix);
+    err = clSetKernelArg<cl_float>(kernel, 6, slope1);
+    err = clSetKernelArg<cl_float>(kernel, 7, slope2);
+    err = clSetKernelArg<cl_float>(kernel, 8, color_a);
+    err = clSetKernelArg<cl_float4>(kernel, 9, color);
+
+    int firstPixelX = boundRect.x() + x;
+    int firstPixelY = boundRect.y() + y;
+    int lastPixelX = firstPixelX + boundRect.width();
+    int lastPixelY = firstPixelY + boundRect.height();
+
+    int ix_start = tile_indice(firstPixelX, TILE_PIXEL_WIDTH);
+    int iy_start = tile_indice(firstPixelY, TILE_PIXEL_HEIGHT);
+
+    int ix_end   = tile_indice(lastPixelX, TILE_PIXEL_WIDTH);
+    int iy_end   = tile_indice(lastPixelY, TILE_PIXEL_HEIGHT);
+
+    for (int iy = iy_start; iy <= iy_end; ++iy)
     {
-        CanvasLayer *layer = surface->strokeContext->layer;
+        const int tileOriginY = iy * TILE_PIXEL_HEIGHT;
+        const int offsetY = std::max(firstPixelY - tileOriginY, 0);
+        const int extraY = std::max(tileOriginY + TILE_PIXEL_HEIGHT - lastPixelY - 1, 0);
 
-        QMatrix transform;
-
-        transform.scale(1.0f, aspect_ratio);
-        transform.scale(1.0f / radius, 1.0f / radius);
-        transform.rotate(-angle);
-
-        QRectF boundRect(-1.0f, -1.0f, 2.0f, 2.0f);
-        boundRect = transform.inverted().mapRect(boundRect).adjusted(-2.0, -2.0, 4.0, 4.0);
-
-        int firstPixelX = boundRect.x() + x;
-        int firstPixelY = boundRect.y() + y;
-        int lastPixelX = firstPixelX + boundRect.width();
-        int lastPixelY = firstPixelY + boundRect.height();
-
-        int ix_start = tile_indice(firstPixelX, TILE_PIXEL_WIDTH);
-        int iy_start = tile_indice(firstPixelY, TILE_PIXEL_HEIGHT);
-
-        int ix_end   = tile_indice(lastPixelX, TILE_PIXEL_WIDTH);
-        int iy_end   = tile_indice(lastPixelY, TILE_PIXEL_HEIGHT);
-
-        cl_kernel kernel;
-        if (lock_alpha > 0.0f)
-            kernel = SharedOpenCL::getSharedOpenCL()->mypaintDabLockedKernel;
-        else
-            kernel = SharedOpenCL::getSharedOpenCL()->mypaintDabKernel;
-
-        cl_int err = CL_SUCCESS;
-        /*
-        float segments[4] =
-            {
-                1.0f, -(1.0f / hardness - 1.0f),
-                hardness / (1.0f - hardness), -hardness / (1.0f - hardness),
-            };
-        */
-        // float color[4] = {color_r, color_g, color_b, color_a};
-        cl_float4 color = {color_r, color_g, color_b, opaque};
-
-        cl_float4 transformMatrix;
-        transformMatrix.s[0] = transform.m11();
-        transformMatrix.s[1] = transform.m21();
-        transformMatrix.s[2] = transform.m12();
-        transformMatrix.s[3] = transform.m22();
-
-        float slope1 = -(1.0f / hardness - 1.0f);
-        float slope2 = -(hardness / (1.0f - hardness));
-
-        err = clSetKernelArg<cl_float>(kernel, 4, hardness);
-        err = clSetKernelArg<cl_float4>(kernel, 5, transformMatrix);
-        err = clSetKernelArg<cl_float>(kernel, 6, slope1);
-        err = clSetKernelArg<cl_float>(kernel, 7, slope2);
-        err = clSetKernelArg<cl_float>(kernel, 8, color_a);
-        err = clSetKernelArg<cl_float4>(kernel, 9, color);
-
-        for (int iy = iy_start; iy <= iy_end; ++iy)
+        for (int ix = ix_start; ix <= ix_end; ++ix)
         {
-            const int tileOriginY = iy * TILE_PIXEL_HEIGHT;
-            const int offsetY = std::max(firstPixelY - tileOriginY, 0);
-            const int extraY = std::max(tileOriginY + TILE_PIXEL_HEIGHT - lastPixelY - 1, 0);
+            const int tileOriginX = ix * TILE_PIXEL_WIDTH;
+            const int offsetX = std::max(firstPixelX - tileOriginX, 0);
+            const int extraX = std::max(tileOriginX + TILE_PIXEL_WIDTH - lastPixelX - 1, 0);
 
-            for (int ix = ix_start; ix <= ix_end; ++ix)
-            {
-                const int tileOriginX = ix * TILE_PIXEL_WIDTH;
-                const int offsetX = std::max(firstPixelX - tileOriginX, 0);
-                const int extraX = std::max(tileOriginX + TILE_PIXEL_WIDTH - lastPixelX - 1, 0);
+            float tileY = (y + 0.5f) - tileOriginY - offsetY;
+            float tileX = (x + 0.5f) - tileOriginX - offsetX;
 
-                float tileY = (y + 0.5f) - tileOriginY - offsetY;
-                float tileX = (x + 0.5f) - tileOriginX - offsetX;
+            int width = TILE_PIXEL_WIDTH - offsetX - extraX;
+            int height = TILE_PIXEL_HEIGHT - offsetY - extraY;
+            cl_int offset = offsetX + offsetY * TILE_PIXEL_WIDTH;
 
-                int width = TILE_PIXEL_WIDTH - offsetX - extraX;
-                int height = TILE_PIXEL_HEIGHT - offsetY - extraY;
-                cl_int offset = offsetX + offsetY * TILE_PIXEL_WIDTH;
+            size_t global_work_size[2] = CL_DIM2(width, height);
+            size_t local_work_size[2] = CL_DIM2(width, 1);
 
-                size_t global_work_size[2] = CL_DIM2(width, height);
-                size_t local_work_size[2] = CL_DIM2(width, 1);
+            surface->strokeContext->priv->modTiles.insert(QPoint(ix, iy));
+            cl_mem data = layer->clOpenTileAt(ix, iy);
 
-                surface->strokeContext->priv->modTiles.insert(QPoint(ix, iy));
-                cl_mem data = layer->clOpenTileAt(ix, iy);
-
-                err = clSetKernelArg<cl_mem>(kernel, 0, data);
-                err = clSetKernelArg<cl_int>(kernel, 1, offset);
-                err = clSetKernelArg<cl_float>(kernel, 2, tileX);
-                err = clSetKernelArg<cl_float>(kernel, 3, tileY);
-                err = clEnqueueNDRangeKernel(SharedOpenCL::getSharedOpenCL()->cmdQueue,
-                                             kernel, 2,
-                                             nullptr, global_work_size, local_work_size,
-                                             0, nullptr, nullptr);
-            }
+            err = clSetKernelArg<cl_mem>(kernel, 0, data);
+            err = clSetKernelArg<cl_int>(kernel, 1, offset);
+            err = clSetKernelArg<cl_float>(kernel, 2, tileX);
+            err = clSetKernelArg<cl_float>(kernel, 3, tileY);
+            err = clEnqueueNDRangeKernel(SharedOpenCL::getSharedOpenCL()->cmdQueue,
+                                         kernel, 2,
+                                         nullptr, global_work_size, local_work_size,
+                                         0, nullptr, nullptr);
         }
-
-        return 1;
     }
 
-    return 0; /* Returns non-zero if the surface was modified */
+    return 1;
 }
 
 static int drawMaskFunction (MyPaintSurface *base_surface,
