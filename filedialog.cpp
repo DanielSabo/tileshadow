4e5a7eecb8106c0a4a4f9f66e7a3c590b13358b5
Daniel Sabo <DanielSabo@gmail.com>
2016-03-07 14:27:46 -0800

    Remember file dialog size & position on Linux

--- filedialog.cpp
+++ filedialog.cpp
@@ -1,11 +1,43 @@
 #include "filedialog.h"
+#include <memory>
+#include <QSettings>
+
+namespace {
+QStringList showFileDialog(QWidget                 *parent,
+                           const QString           &caption,
+                           const QString           &dir,
+                           const QString           &filter,
+                           QFileDialog::Options     options,
+                           QFileDialog::AcceptMode  acceptMode,
+                           QFileDialog::FileMode    fileMode)
+{
+    std::unique_ptr<QFileDialog> dialog{new QFileDialog{parent, caption, dir, filter}};
+    dialog->setAcceptMode(acceptMode);
+    dialog->setFileMode(fileMode);
+#ifdef Q_OS_LINUX
+    // Disable native file dialogs due to KDE Bug 357684
+    dialog->setOptions(options | QFileDialog::DontUseNativeDialog);
+
+    QSettings settings;
+    QByteArray geometry = settings.value("FileDialog/geometry").toByteArray();
+    if (!geometry.isEmpty())
+        dialog->restoreGeometry(geometry);
+#else
+    dialog->setOptions(options);
+#endif
+
+    QStringList result;
+    if (dialog->exec() == QDialog::Accepted)
+        result = dialog->selectedFiles();
 
 #ifdef Q_OS_LINUX
-// Disable native file dialogs due to KDE Bug 357684
-#define ADDITIONAL_OPITONS QFileDialog::DontUseNativeDialog;
-#else
-#define ADDITIONAL_OPITONS QFileDialog::Option();
+    settings.setValue("FileDialog/geometry", dialog->saveGeometry());
 #endif
+
+    return result;
+}
+}
+
 
 QString FileDialog::getSaveFileName(QWidget              *parent,
                                     const QString        &caption,
@@ -14,8 +46,7 @@ QString FileDialog::getSaveFileName(QWidget              *parent,
                                     QString              *selectedFilter,
                                     QFileDialog::Options  options)
 {
-    options |= ADDITIONAL_OPITONS;
-    return QFileDialog::getSaveFileName(parent, caption, dir, filter, selectedFilter, options);
+    return showFileDialog(parent, caption, dir, filter, options, QFileDialog::AcceptSave, QFileDialog::AnyFile).value(0);
 }
 
 QString FileDialog::getOpenFileName(QWidget              *parent,
@@ -25,8 +56,7 @@ QString FileDialog::getOpenFileName(QWidget              *parent,
                                     QString              *selectedFilter,
                                     QFileDialog::Options  options)
 {
-    options |= ADDITIONAL_OPITONS;
-    return QFileDialog::getOpenFileName(parent, caption, dir, filter, selectedFilter, options);
+    return showFileDialog(parent, caption, dir, filter, options, QFileDialog::AcceptOpen, QFileDialog::ExistingFile).value(0);
 }
 
 QStringList FileDialog::getOpenFileNames(QWidget              *parent,
@@ -36,6 +66,5 @@ QStringList FileDialog::getOpenFileNames(QWidget              *parent,
                                          QString              *selectedFilter,
                                          QFileDialog::Options  options)
 {
-    options |= ADDITIONAL_OPITONS;
-    return QFileDialog::getOpenFileNames(parent, caption, dir, filter, selectedFilter, options);
+    return showFileDialog(parent, caption, dir, filter, options, QFileDialog::AcceptOpen, QFileDialog::ExistingFiles);
 }
